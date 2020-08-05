5c7de20d4a36218ded68bc4c0ff91f23490ac6f0
cloudsample6 <cloudsample6@gmail.com>
2020-08-06 02:20:25 +0700

    change assets images logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,7 @@
                 <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                     <div class="p-4 m-3">
                         <!-- <img src="../assets/img/stisla-fill.svg" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2"> -->
-                        <img src="../assets/img/logo.png" alt="logo" width="80" class="shadow-light rounded mb-5 mt-2">
+                        <img src="../assets/img/login-logo.png" alt="logo" width="50" class="shadow-light rounded mb-5 mt-2">
                         <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">RTRW Pay</span></h4>
                         <p class="text-muted">Before you get started, you must login or register if you don't already have an account.</p>
                         <form method="POST" action="{{ route('login-post') }}" class="needs-validation" novalidate="">
@@ -93,7 +93,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/unsplash/login-bg.jpg">
+                <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="../assets/img/unsplash/login-bg-city-2.jpg">
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
